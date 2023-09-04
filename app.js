77eb5e673129efa1ffcd99b36b781e42dbaf96c9
Vinay Jain <f20190728@pilani.bits-pilani.ac.in>
2023-09-04 16:54:35 +0530

    Add get request /makecampground

--- app.js
+++ app.js
@@ -28,6 +28,11 @@ app.set('views', path.join(__dirname, 'views'));
 app.get('/', (req, res) => {
     res.render('home')
 })
+app.get('/makecampground', async (req, res) => {
+    const camp = new Campground({ title: 'My Backyard', description: 'cheap camping !!'});
+    await camp.save();
+    res.send(camp);
+})
 
 app.listen(3000, () => {
     console.log('Serving on port 3000')
