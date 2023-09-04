d91a837075735e602bebb91af593b26520375756
Vinay Jain <f20190728@pilani.bits-pilani.ac.in>
2023-09-04 16:50:06 +0530

    First commit

---
+++ app.js
@@ -0,0 +1,46 @@
+const express = require('express');
+const path = require('path');
+const mongoose = require('mongoose');
+const Campground = require('./models/campground');
+
+mongoose.connect('mongodb://127.0.0.1:27017/yelp-camp', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+})
+// .then(()=>{
+//     console.log("CONNECTION OPEN!!");
+// }).catch(err => {
+//     console.log("OH NO ERROR!!!!");
+//     console.log(err);
+// });
+
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "connection error:"));
+db.once("open", () => {
+    console.log("Database connected");
+})
+
+const app = express();
+
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
+app.get('/', (req, res) => {
+    res.render('home')
+})
+app.get('/makecampground', async (req, res) => {
+    const camp = new Campground({ title: 'My Backyard', description: 'cheap camping !!'});
+    await camp.save();
+    res.send(camp);
+})
+
+app.listen(3000, () => {
+    console.log('Serving on port 3000')
+})
+
+
+
+// EJS - Embedded Javascript
+// views - for ejs
+// models = for mongoose (Mongoose is a MongoDB ODM i.e (Object database Modelling) that used to translate the code and its representation from MongoDB to the Node. js server.)
+// The app.set() function is used to assign the setting name to value. You may store any value that you want, but certain names can be used to configure the behavior of the server. 
